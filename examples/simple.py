02c9ed9c05fbae5b88787b7543e340a531db5091
Lukas Voegl <lukas@voegl.org>
2025-02-19 18:19:21 +0100

    feat(README): Add pictured examples

--- examples/simple.py
+++ examples/simple.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# The above lines are only required to run in the repository's examples directory
+
 from gmaps_route.route.destination import Destination
 from gmaps_route.route.map import Map
 from gmaps_route.route.route import Route
