32e2e9cffa3705c846c82d325f52be3f7a6d0518
Lukas Voegl <lukas@voegl.org>
2026-04-02 22:38:21 +0200

    fix(isort): Reformat examples to match style rules

--- examples/large.py
+++ examples/large.py
@@ -1,9 +1,11 @@
+# fmt: off
 import os
 import sys
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # The above lines are only required to run in the repository's examples directory
+# fmt: on
 
 from gmaps_route.route.destination import Destination
 from gmaps_route.route.map import Map
@@ -12,44 +14,49 @@ from gmaps_route.route.subdestination import Subdestination
 from gmaps_route.route.transportation import Transportation
 
 
-route_map = Map(Map.Type.SATELLITE)
-transportation = Transportation(Transportation.Type.CAR)
+def main() -> None:
+    route_map = Map(Map.Type.SATELLITE)
+    transportation = Transportation(Transportation.Type.CAR)
 
-start = Destination.Builder().latitude(48.1351253).longitude(11.5819805).build()
-intermediate = (
-    Destination.Builder()
-    .latitude(48.7758459)
-    .longitude(9.1829321)
-    .add_subdestination(
-        # note that subdestinations between 'start' and 'intermediate' must be added to 'intermediate'
-        Subdestination.Builder()
-        .latitude(48.127631)
-        .longitude(11.431652)
+    start = Destination.Builder().latitude(48.1351253).longitude(11.5819805).build()
+    intermediate = (
+        Destination.Builder()
+        .latitude(48.7758459)
+        .longitude(9.1829321)
+        .add_subdestination(
+            # note that subdestinations between 'start' and 'intermediate' must be added to 'intermediate'
+            Subdestination.Builder()
+            .latitude(48.127631)
+            .longitude(11.431652)
+            .build()
+        )
+        .add_subdestination(
+            Subdestination.Builder().latitude(48.4040736).longitude(9.999808).build()
+        )
         .build()
     )
-    .add_subdestination(
-        Subdestination.Builder().latitude(48.4040736).longitude(9.999808).build()
-    )
-    .build()
-)
 
-end = (
-    Destination.Builder()
-    .latitude(50.1109221)
-    .longitude(8.6821267)
-    .add_subdestination(
-        Subdestination.Builder().latitude(49.4816004).longitude(8.4401422).build()
+    end = (
+        Destination.Builder()
+        .latitude(50.1109221)
+        .longitude(8.6821267)
+        .add_subdestination(
+            Subdestination.Builder().latitude(49.4816004).longitude(8.4401422).build()
+        )
+        .build()
     )
-    .build()
-)
 
-route = (
-    Route.Builder()
-    .map(route_map)
-    .transportation(transportation)
-    .add_destination(start)
-    .add_destination(intermediate)
-    .add_destination(end)
-    .build()
-)
-print(route.url)
+    route = (
+        Route.Builder()
+        .map(route_map)
+        .transportation(transportation)
+        .add_destination(start)
+        .add_destination(intermediate)
+        .add_destination(end)
+        .build()
+    )
+    print(route.url)
+
+
+if __name__ == "__main__":
+    main()
--- examples/simple.py
+++ examples/simple.py
@@ -1,9 +1,11 @@
+# fmt: off
 import os
 import sys
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # The above lines are only required to run in the repository's examples directory
+# fmt: on
 
 from gmaps_route.route.destination import Destination
 from gmaps_route.route.map import Map
@@ -12,8 +14,13 @@ from gmaps_route.route.subdestination import Subdestination
 from gmaps_route.route.transportation import Transportation
 
 
-start = Destination.Builder().latitude(48.1351253).longitude(11.5819805).build()
-end = Destination.Builder().latitude(50.1109221).longitude(8.6821267).build()
+def main() -> None:
+    start = Destination.Builder().latitude(48.1351253).longitude(11.5819805).build()
+    end = Destination.Builder().latitude(50.1109221).longitude(8.6821267).build()
 
-route = Route.Builder().add_destination(start).add_destination(end).build()
-print(route.url)
+    route = Route.Builder().add_destination(start).add_destination(end).build()
+    print(route.url)
+
+
+if __name__ == "__main__":
+    main()
